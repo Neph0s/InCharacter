138edd14e5b20d2961f83acd34f1ef43213aa55a
Neph0s <xtwang21@m.fudan.edu.cn>
2024-01-11 20:00:22 +0800

    update questionnaire

--- code/test_eval_methods.py
+++ code/test_eval_methods.py
@@ -1,40 +1,20 @@
 from personality_tests import personality_assessment
 
-characters = ['hutao-zh']
+characters = ['wangduoyu-zh', "xiaofeng-zh", "tongxiangyu-zh", "haruhi-zh", 'Snape-en', 'hutao-zh']
 
-for character in characters:
-    # direct
-    # personality_assessment(
-    #     character, 'ChatHaruhi', 'gpt-4', 
-    #     '16Personalities', 'direct')
+
+
     
+for repeat_times in [0.25, 1, 2]:
+    for eval_method in [#'direct', 
+                        'choose', 
+                        'interview_convert', 'interview_convert_api'
+                        'interview_assess_batch', 'interview_assess_collective']:
+        for character in characters:
+            personality_assessment(
+                character, 'ChatHaruhi', 'gpt-3.5', 
+                '16Personalities', eval_method, 'gpt-4', repeat_times=repeat_times)
 
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'interview_assess_collective', 'gpt-4', repeat_times=0.25)
-
-    import pdb; pdb.set_trace()
+    
         
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'choose', 'gpt-4', repeat_times=0.25)
     
-    personality_assessment(
-        'Snape-en', 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'choose', 'gpt-4', repeat_times=0.25)
-    
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'interview_convert_api')
-
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'interview_convert', 'gpt-4')
-    
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'interview_assess_batch', 'gpt-4')
-
-    personality_assessment(
-        character, 'ChatHaruhi', 'gpt-4', 
-        '16Personalities', 'interview_assess_collective', 'gpt-4')
